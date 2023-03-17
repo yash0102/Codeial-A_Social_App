39a5d730f5afc6c24ee4659c691915ad3a0f6d02
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-18 00:14:45 +0530

    check Authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express'); 
 // Express create onces on main index.js file , it just fetch the existing instace
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 const { route } = require('./users');
 
-router.post('/create',postsController.create);
+router.post('/create',passport.ckeckAuthentication ,postsController.create);
 
 module.exports = router;
