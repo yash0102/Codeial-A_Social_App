9a94df784350deca67425b0faacf790a7bf3903b
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-17 23:18:57 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
-module.exports.home = function(req ,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
+const Post = require('../models/post');
+
+module.exports.home = (req ,res)=>{
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+    
+    // populate the user of each post ?
+   Post.find({}).populate('user') // populate the `user` field with the corresponding user document
+   .then((posts)=>{
     return res.render('home',{
-        title: "Home"
-    }); 
+        title: 'Codeail | Home',
+        posts: posts
+    })
+   })
 }
+
+// this is the old version
+//    Post.find({},(err,posts)=>{
+//     return res.render('home',{
+//         title: 'Codeail | Home',
+//         posts: posts
+//     })
+
 
 // module.exports.actionName = function(req,res){}
