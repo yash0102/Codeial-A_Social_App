493803cd56d1c789f88e1e915cfe177e169ae130
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-19 01:47:53 +0530

    Add Comments to it

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Required modules are imported and an instance of express server created.
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
@@ -6,8 +7,10 @@ const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
+// Passport is a JS library used to manage user authentication related concerns
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+// Connect Mongo is a popular middleware designed to manage MongoDB sessions with ExpressJS
 const MongoStore = require('connect-mongo');
 
 
@@ -26,14 +29,18 @@ const MongoStore = require('connect-mongo');
 
 
 
+// Middleware to use during development
 
-
+// parsed request's body with urlencoded payload format (for reading input as key value pair)
+// from the incoming requests and made it accessible to req.body object of the response object. 
+// It is using Express inbuilt middleware for it.
 app.use(express.urlencoded()); // added a parser,
 app.use(cookieParser());
 
+// to serve static files stored in the ./assets directory 
 app.use(express.static('./assets'));
 
-app.use(expressLayouts);
+app.use(expressLayouts); // to use a default layout
 
 //extract style and script from sub pages into layout 
 app.set('layout extractStyles',true);
@@ -41,7 +48,7 @@ app.set('layout extractScripts',true);
 
 
 // set up the view engine
-app.set('view engine','ejs');
+app.set('view engine','ejs'); // EJS templating engine used to render HTML templates
 app.set('views','./views');
 
 // mongo store is used to store the session cookie in the db
@@ -52,7 +59,7 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100) // in mili second
+        maxAge: (1000 * 60 * 100) // cookie expiry set in milliseconds. 
     },
     store: MongoStore.create(
         {    
@@ -66,13 +73,14 @@ app.use(session({
     )
 }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+ 
+app.use(passport.initialize()); // Passport Initialize middleware starts for authentication
+app.use(passport.session());  // Creates a login session for users using Passport Middlware.
 
-
+// Calling setAuthenticatedUser on every request allwos only authenticated users to access certain routes/routes that require authentication
 app.use(passport.setAuthenticatedUser);
 
-// use express router
+// use express router , to rendere various routes
 app.use('/', require('./routes'));// any request comes, it requires
 
 
