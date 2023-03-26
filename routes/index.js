2609b656a4ae0d195ba24c503a4263502b713e09
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-03-27 05:04:42 +0530

    Setting up directory structure , rendering an API

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.use('/users',require('./users')); // any request come from "/users" it re
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 
+router.use('/api' , require('./api'));
 
 // for any further routes , access from here
 // router.use('/routerName',require('./routerFile'));
