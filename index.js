f1db1c636c6e4bda14ec2b9f01768628cffaa931
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-04-02 01:57:57 +0530

    Using google auth : with a link

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 18000;
+const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
@@ -11,6 +11,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
 // Connect Mongo is a popular middleware designed to manage MongoDB sessions with ExpressJS
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
