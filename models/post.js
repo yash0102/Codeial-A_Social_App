5a97787795019d2c8f743e2270ab8308754457b8
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-04-04 16:03:25 +0530

    Schema Setup :: Likes

--- models/post.js
+++ models/post.js
@@ -10,10 +10,18 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
     },
     // include the array of ids of all comments in this post schema itself
-    comments : [{
+    comments : [
+        {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment'
-}]
+        }
+    ],
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like' 
+        }
+    ]
 },{
     timestamps: true // it shows a created and updated time
 });
