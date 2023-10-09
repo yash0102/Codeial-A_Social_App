b5883074069d82143bff79ee93286f7ea4261acb
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-10-09 10:52:13 +0530

    Changes in gulpfile

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -12,17 +12,17 @@ import imagemin from "gulp-imagemin";
 gulp.task("css", function (done) {
   console.log("Minifying css...");
   gulp
-    .src("./assets/scss/**/**.scss")
-    .pipe(sass())
-    .pipe(cssnano())
+    .src("./assets/scss/**/*.scss") //means it select all the .scss file inside scss folder
+    .pipe(sass())  // compile scss to css
+    .pipe(cssnano()) // minifying css
     .pipe(gulp.dest("./assets.css"));
 
   gulp
     .src("./assets/**/*.css")
-    .pipe(rev())
+    .pipe(rev())   // generate unique hash file name , 432gddk.css
     .pipe(gulp.dest("./public/assets"))
     .pipe(
-      rev.manifest({
+      rev.manifest({   // maps the file name with hashs , home-432gddk.css
         cwd: "public",
         merge: true,
       })
@@ -35,7 +35,7 @@ gulp.task("js", function (done) {
   console.log("minifying js...");
   gulp
     .src("./assets/**/*.js")
-    .pipe(uglify())
+    .pipe(uglify())  // minifying the js file
     .pipe(rev())
     .pipe(gulp.dest("./public/assets"))
     .pipe(
@@ -78,81 +78,3 @@ gulp.task(
     done();
   }
 );
-
-// const gulp = require("gulp");
-// const sass = require("gulp-sass")(require("sass"));
-// const cssnano = require("gulp-cssnano");
-// const rev = require("gulp-rev");
-// const uglify = require("gulp-uglify-es").default;
-// const imagemin = require("gulp-imagemin");
-
-// gulp.task("css", function (done) {
-//   console.log("minifying css...");
-//   gulp
-//     .src("./assets/sass/**/*/.scss")
-//     .pipe(sass())
-//     .pipe(cssnano())
-//     .pipe(gulp.dest("./assets.css"));
-
-//   gulp
-//     .src("./assets/**/*/.css")
-//     .pipe(rev())
-//     .pipe(gulp.dest("./public/assets"))
-//     .pipe(
-//       rev.manifest({
-//         cwd: "public",
-//         merge: true,
-//       })
-//     )
-//     .pipe(gulp.dest("./public/assets"));
-//   done();
-// });
-
-// gulp.task("js", function (done) {
-//   console.log("minifying js...");
-//   gulp
-//     .src("./assets/**/*/.js")
-//     .pipe(uglify())
-//     .pipe(rev())
-//     .pipe(gulp.dest("./public/assets"))
-//     .pipe(
-//       rev.manifest({
-//         cwd: "public",
-//         merge: true,
-//       })
-//     )
-//     .pipe(gulp.dest("./public/assets"));
-//   done();
-// });
-
-// gulp.task("images", function (done) {
-//   console.log("compressing images...");
-//   gulp
-//     .src("./assets/**/*/.+(png|jpg|gif|svg|jpeg)")
-//     .pipe(imagemin())
-//     .pipe(rev())
-//     .pipe(gulp.dest("./public/assets"))
-//     .pipe(
-//       rev.manifest({
-//         cwd: "public",
-//         merge: true,
-//       })
-//     )
-//     .pipe(gulp.dest("./public/assets"));
-//   done();
-// });
-
-// // empty the public/assets directory
-// gulp.task("clean:assets", function (done) {
-//   // del.sync('./public/assets');
-//   done();
-// });
-
-// gulp.task(
-//   "build",
-//   gulp.series("clean:assets", "css", "js", "images"),
-//   function (done) {
-//     console.log("Building assets");
-//     done();
-//   }
-// );
